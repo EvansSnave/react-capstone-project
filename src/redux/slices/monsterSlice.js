53cfef1b8742b983b4652a7c12e3705fd86abbac
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-09-19 22:39:07 -0500

    install axios

---
+++ src/redux/slices/monsterSlice.js
@@ -0,0 +1,23 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const fetchMonstersAPI = createAsyncThunk('monsters/fetchMonstersAPI', async () => {
+  try {
+    const response = await axios.get('https://mhw-db.com/monsters');
+    return response.data;
+  } catch (error) {
+    throw new Error('Something went wrong.');
+  }
+});
+
+const monsters = createSlice({
+  name: 'monsters',
+  initialState: {},
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMonstersAPI.fulfilled, (state, action) => {
+
+      })
+  }
+});
