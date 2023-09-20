4d992e58be7cf8d4d6d506c8cfc675a60560c459
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-09-19 22:05:54 -0500

    install react bootstrap

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,23 @@
+const HomePage = () => {
+  return (
+    <main>
+      <div>
+        <img></img>
+        <h1>Monster Hunter data</h1>
+      </div>
+      <div>
+        <div>Large monsters</div>
+        <div>
+          <div>
+            <img></img>
+            <div>
+              <img></img>
+              <h2></h2>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,3 +9,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+//https://docs.mhw-db.com/#monsters
