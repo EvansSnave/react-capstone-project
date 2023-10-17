a379eb24675adec45b7b4b509b7de25c777da838
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-10-17 14:05:41 -0500

    Updates

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import pollutionReducer from './slice';
-
-export default configureStore({
-  reducer: {
-    pollution: pollutionReducer,
-  },
-});
