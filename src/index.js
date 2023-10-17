d23e235140cd90f5c7f0431ae3f88eac6ccbe434
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-10-17 15:05:05 -0500

    change basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/react-capstone-project">
       <Provider store={store}>
         <App />
       </Provider>
